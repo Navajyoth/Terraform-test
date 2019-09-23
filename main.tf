623d52b844c4a93e9d01ba208f6affdf3615aaf7
Ubuntu <ubuntu@ip-172-31-6-45.ap-south-1.compute.internal>
2019-09-23 12:51:14 +0000

    terraform main file added

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  access_key = "AKIATUS6W5NQY6VGN6FW"
+  secret_key = "rMvhvOQCCpObKM7goDvMqdEguF7VDMfIjZfZxRdq"
+  region = "ap-south-1"
+}
+resource "aws_instance" "example" {
+  ami           = "ami-009110a2bf8d7dd0a"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Terraform AWS demo test, !!!!!" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+tags = {
+Name = "terraform-aws-demo" 
+}
+}
+resource "aws_security_group" "instance" {
+  name = "terraform-aws-demo-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
